fef1ba69f8e2d95301bc65151948aaca524b2515
springfly13 <innasavelii@hotmail.com>
2016-07-29 16:24:00 +0200

    Reglage du probleme de decalage horaire dans les tests

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -16,7 +16,7 @@ public class RestaurantTest {
     public void testDefaultOpeningHoursValidTimeValidDay() {
        Restaurant myRestaurant = new Restaurant();
        assertNotNull( myRestaurant.getClock() );
-       Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T15:00:00Z"), ZoneId.systemDefault());
+       Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T15:01:00Z"), ZoneId.of("UTC+0"));
        myRestaurant.setClock(clockHour);
        assertTrue(myRestaurant.isOpen(DayOfWeek.FRIDAY ));
     }
@@ -25,7 +25,7 @@ public class RestaurantTest {
     public void testDefaultOpeningHoursFalseDay() {
         Restaurant myRestaurant = new Restaurant();
         assertNotNull( myRestaurant.getClock() );
-        Clock clockHour = Clock.fixed(Instant.parse("2016-07-29T15:00:00Z"), ZoneId.systemDefault());
+        Clock clockHour = Clock.fixed(Instant.parse("2016-07-29T15:00:00Z"), ZoneId.of("UTC+0"));
         myRestaurant.setClock(clockHour);
         assertFalse(myRestaurant.isOpen(DayOfWeek.SUNDAY));
     }
@@ -34,17 +34,34 @@ public class RestaurantTest {
     public void testDefaultOpeningHoursEvening() {
         Restaurant myRestaurant = new Restaurant();
         assertNotNull( myRestaurant.getClock() );
-        Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T18:30:00Z"), ZoneId.systemDefault());
+        Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T18:01:00Z"), ZoneId.of("UTC+0"));
         myRestaurant.setClock(clockHour);
         assertFalse(myRestaurant.isOpen(DayOfWeek.TUESDAY));
+    }
+
+    @Test
+    public void testDefaultOpeningHoursEvening2() {
+        Restaurant myRestaurant = new Restaurant();
+        assertNotNull( myRestaurant.getClock() );
+        Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T17:59:00Z"), ZoneId.of("UTC+0"));
+        myRestaurant.setClock(clockHour);
+        assertTrue(myRestaurant.isOpen(DayOfWeek.TUESDAY));
     }
 
     @Test
     public void testDefaultOpeningHoursMorning() {
         Restaurant myRestaurant = new Restaurant();
         assertNotNull( myRestaurant.getClock() );
-        Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T06:30:00Z"), ZoneId.systemDefault());
+        Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T08:59:00Z"), ZoneId.of("UTC+0"));
         myRestaurant.setClock(clockHour);
         assertFalse(myRestaurant.isOpen(DayOfWeek.WEDNESDAY ));
+    }
+
+    public void testDefaultOpeningHoursMorning2() {
+        Restaurant myRestaurant = new Restaurant();
+        assertNotNull( myRestaurant.getClock() );
+        Clock clockHour = Clock.fixed(Instant.parse("2016-07-28T09:01:00Z"), ZoneId.of("UTC+0"));
+        myRestaurant.setClock(clockHour);
+        assertTrue(myRestaurant.isOpen(DayOfWeek.WEDNESDAY ));
     }
 }
